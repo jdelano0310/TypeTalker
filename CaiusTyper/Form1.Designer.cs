c7d5557eff3628f5813b5f4934524153b660c73b
Jon <jd31068@hotmail.com>
2024-04-10 10:57:15 -0400

    Add project files.

---
+++ CaiusTyper/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace CaiusTyper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDisplayPressedKey = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblDisplayPressedKey
+            // 
+            this.lblDisplayPressedKey.BackColor = System.Drawing.Color.Transparent;
+            this.lblDisplayPressedKey.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblDisplayPressedKey.Font = new System.Drawing.Font("Microsoft Sans Serif", 52F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDisplayPressedKey.Location = new System.Drawing.Point(0, 0);
+            this.lblDisplayPressedKey.Name = "lblDisplayPressedKey";
+            this.lblDisplayPressedKey.Size = new System.Drawing.Size(749, 577);
+            this.lblDisplayPressedKey.TabIndex = 0;
+            this.lblDisplayPressedKey.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblDisplayPressedKey.MouseDown += new System.Windows.Forms.MouseEventHandler(this.lblDisplayPressedKey_MouseDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(749, 577);
+            this.ControlBox = false;
+            this.Controls.Add(this.lblDisplayPressedKey);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Type Talker";
+            this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDisplayPressedKey;
+    }
+}
+
---
+++ CaiusTyper/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Speech.Synthesis;
+using System.Windows.Forms;
+
+namespace CaiusTyper
+{
+    public partial class Form1 : Form
+    {
+        private SpeechSynthesizer TextToSpeech;
+        private String SayWhat;
+        private Random rnd;
+        private int JustSounds = 0; // don't hold more than 4 sounds in clue
+        private string[] numberWord = new string[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+        private string lastFourNumbers;
+
+        private Utilities.globalKeyboardHook gkh = new Utilities.globalKeyboardHook();
+
+        // GbelowC = 196, A = 220, Asharp = 233, B = 247, C = 262, Csharp = 277, D = 294, Dsharp = 311, E = 330, F = 349, Fsharp = 370,
+        // G = 392, Gsharp = 415
+        private Int16[] tones = new Int16[] { 196, 220, 233, 247, 262, 277, 294, 311, 330, 349, 370, 392, 415 };
+
+        // WHOLE = 1600, HALF = WHOLE/2, QUARTER = HALF/2, EIGHTH = QUARTER/2, SIXTEENTH = EIGHTH/2
+        private Int16[] duration = new short[] { 1600, 800, 400, 200, 100 };
+
+        private void Speak()
+        {
+            Application.DoEvents();
+            TextToSpeech.SpeakAsyncCancelAll();
+            Application.DoEvents();
+            TextToSpeech.SpeakAsync(SayWhat);
+            Application.DoEvents();
+            JustSounds -= 1;
+        }
+
+
+        private void DisplayRandomColor()
+        {
+            Application.DoEvents();
+            rnd = new Random();
+            this.BackColor = Color.FromArgb(255, rnd.Next(255), rnd.Next(255), rnd.Next(255));
+            Application.DoEvents();
+        }
+
+        private void PlayRandomSound()
+        {
+            // plays a tone when a key is pressed that doesn't have an associated letter
+            Int32 toneIdx = 0;
+            Int32 durationIdx = 0;
+
+            Application.DoEvents();
+            rnd = new Random();
+            toneIdx = rnd.Next(12);
+            durationIdx = rnd.Next(4);
+
+            Console.Beep(tones[toneIdx], duration[durationIdx]);
+
+        }
+
+        private void IgnoreTheseKeys()
+        {
+
+            // these keys will be ignored while the program is running
+            gkh.HookedKeys.Add(Keys.LMenu); // alt key
+            gkh.HookedKeys.Add(Keys.RMenu);
+
+            gkh.HookedKeys.Add(Keys.LShiftKey); // shift key
+            gkh.HookedKeys.Add(Keys.RShiftKey);
+
+            gkh.HookedKeys.Add(Keys.LWin);  // windows key
+            gkh.HookedKeys.Add(Keys.RWin);  
+
+            gkh.HookedKeys.Add(Keys.Tab); // tab key
+            
+            gkh.HookedKeys.Add(Keys.Menu); // menu key
+
+        }
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            TextToSpeech = new SpeechSynthesizer
+            {
+                Rate = -2
+            };
+
+            IgnoreTheseKeys();
+
+            gkh.KeyDown += new KeyEventHandler(gkh_KeyDown);
+            gkh.KeyUp += new KeyEventHandler(gkh_KeyUp);
+
+        }
+
+        void gkh_KeyUp(object sender, KeyEventArgs e)
+        {
+            lblDisplayPressedKey.Text = "";
+            e.Handled = true;
+        }
+
+        void gkh_KeyDown(object sender, KeyEventArgs e)
+        {
+            lblDisplayPressedKey.Text = "";
+            e.Handled = true;
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+            Int32 keyCode = Convert.ToInt32(e.KeyChar);
+            bool keyIsDisplayable = (keyCode > 47 && keyCode < 58) | (keyCode > 64 && keyCode < 91) | (keyCode > 96 && keyCode < 123);
+
+            DisplayRandomColor();
+
+            if (keyIsDisplayable)
+            {
+                SayWhat = e.KeyChar.ToString();
+
+                if (keyCode > 47 && keyCode < 58)
+                {
+                    // added a pin to force close the program
+                    lastFourNumbers += SayWhat;
+
+                    // if a number is pressed, also spell the word out on screen
+                    lblDisplayPressedKey.Text = SayWhat + "\n(" + numberWord[int.Parse(SayWhat)] + ")";
+                }
+                else
+                {
+                    lastFourNumbers = "";
+
+                    // speak what was typed
+                    lblDisplayPressedKey.Text = SayWhat;
+                }
+                Speak();
+
+                if (lastFourNumbers == "7391")
+                {
+                    this.Close();  // force the window to close
+                }
+                else
+                {
+                    if (lastFourNumbers.Length == 4) { lastFourNumbers = ""; }
+                }
+            }
+            else
+            {
+                if (JustSounds < 3)
+                {
+                    JustSounds += 1;
+                    lblDisplayPressedKey.Text = "";
+                    PlayRandomSound();
+                }
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                TextToSpeech.Dispose();
+                gkh.unhook();
+            }
+            catch {
+
+            }
+
+        }
+
+        private void lblDisplayPressedKey_MouseDown(object sender, MouseEventArgs e)
+        {
+            lblDisplayPressedKey.Text = "";
+            SayWhat = "mouse click";
+
+            DisplayRandomColor();
+            Speak();
+        }
+
+    }
+}
